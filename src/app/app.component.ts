2b48e58d969303542fbc379e42d4580d38572234
Rohit Singla <Rohikacreations@gmail.com>
2025-03-21 18:50:30 +0530

    Added Information on Homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { VisitService } from './visitors/services/visit.service';
 import { NotificationComponent } from './core/notification/notification.component';
+import { CurrencyPipe, DatePipe } from '@angular/common';
+import { ReportService } from './components/services/report.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterModule, NotificationComponent],
+  imports: [RouterOutlet, RouterLink, RouterModule, NotificationComponent,DatePipe,CurrencyPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
@@ -15,14 +17,45 @@ export class AppComponent  implements OnInit{
   title = 'rohika-billing';
   visitCount:any = '';
 
-  constructor(private visitService:VisitService) {
+  currentDate: Date = new Date();
+
+  monthMap:Map<string, number>=new Map<string, number>();
+
+  currentMonthSales: number=0;
+
+  constructor(private reportService:ReportService) {
 
   }
 
   ngOnInit(): void {
-    this.visitService.getTotalVisitCount().subscribe(count => {
-      this.visitService.incrementVisit();
-      this.visitCount = count; // Set visit count when data is fetched
+    this.getReportData();
+  }
+  
+  getReportData() {
+    this.reportService.getReport().subscribe({
+      next: (response) => {
+        // Get current year and month dynamically
+        const currentYear = new Date().getFullYear();
+        const currentMonth = String(new Date().getMonth() + 1).padStart(2, '0');
+        const currentFormattedMonth = `${currentYear}-${currentMonth}`;
+  
+        // Generate all months of the year with default values
+        for (let i = 1; i <= 12; i++) {
+          const formattedMonth = `${currentYear}-${String(i).padStart(2, '0')}`;
+          this.monthMap.set(formattedMonth, 0);
+        }
+  
+        // Fill in data from JSON
+        response.forEach((entry: { month: string; totalAmount: number }) => {
+          this.monthMap.set(entry.month, entry.totalAmount);
+        });
+  
+        // Assign the current month's sales
+        this.currentMonthSales = this.monthMap.get(currentFormattedMonth) || 0;
+      },
+      error: (error) => {
+        console.error('Error fetching report data', error);
+      }
     });
   }
   
